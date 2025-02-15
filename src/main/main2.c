ab893c1645b499a86bef26b33174cf8a0e17ceff
someone2639 <someone2639@gmail.com>
2025-02-14 22:30:39 -0500

    more freeing from tk

--- src/main/main2.c
+++ src/main/main2.c
@@ -8,13 +8,14 @@
 OSTask hvqtask;
 static OSMesgQueue spMesgQ;
 static OSMesg spMesgBuf;
+
 #define VIDEO_DMA_MSG_SIZE 1
 static OSIoMesg videoDmaMesgBlock;
 static OSMesgQueue videoDmaMessageQ;
 static OSMesg videoDmaMessages[VIDEO_DMA_MSG_SIZE];
 
-OSTask hvqtask;     /* RSP task data */
-HVQM2Arg hvq_sparg; /* Parameter for the HVQM2 microcode */
+OSTask hvqtask;     // RSP task data
+HVQM2Arg hvq_sparg; // Parameter for the HVQM2 microcode
 
 u8 hvqm_headerBuf[sizeof(HVQM2Header) + 16];
 
@@ -43,32 +44,29 @@ u8 *get_record(HVQM2Record *headerbuf, void *bodybuf, u16 type, u8 *stream, OSIo
     return stream;
 }
 
+u64 getTime() {
+    static u64 last_time = 0;
+
+    return OS_CYCLES_TO_USEC(osGetTime() - last_time);
+}
+
 void Main(void *argument) {
     HVQM2Header *hvqm_header;
-    int h_offset, v_offset; /* Position of image display */
-    int screen_offset;      /* Number of pixels from start of frame buffer to display position */
+    int h_offset, v_offset; // Position of image display
+    int screen_offset;      // Number of pixels from start of frame buffer to display position
 
     hvqm_header = OS_DCACHE_ROUNDUP_ADDR(hvqm_headerBuf);
 
-    /*
-     * Acquire an SP event (if using the RSP version of the decoder)  */
+    // Acquire an SP event (if using the RSP version of the decoder)
     osCreateMesgQueue(&spMesgQ, &spMesgBuf, 1);
     osSetEventMesg(OS_EVENT_SP, &spMesgQ, NULL);
-    /*
-     * Create DMA message queue for the reading in of video records
-     */
+
+    // Create DMA message queue for the reading in of video records
     osCreateMesgQueue(&videoDmaMessageQ, videoDmaMessages, VIDEO_DMA_MSG_SIZE);
 
-    /*
-     * Create the timekeeper thread
-     */
-    // createTimekeeper();
-
-    /*
-     * Initialize the HVQM2 decoder
-     */
-    /* If using the RSP version of the decoder */
-    /* also setup the RSP task data next */
+    // Initialize the HVQM2 decoder
+    // If using the RSP version of the decoder
+    // also setup the RSP task data next
     hvqm2InitSP1(0xff);
     hvqtask.t.ucode = (u64 *) hvqm2sp1TextStart;
     hvqtask.t.ucode_size = (int) hvqm2sp1TextEnd - (int) hvqm2sp1TextStart;
@@ -83,22 +81,13 @@ void Main(void *argument) {
     hvqtask.t.yield_data_size = HVQM2_YIELD_DATA_SIZE;
 
 
-    /*
-     * Initialize the frame buffer (clear buffer contents and status flag)
-     */
+    // Initialize the frame buffer (clear buffer contents and status flag)
     init_cfb();
     osViSwapBuffer(cfb[NUM_CFBs - 1]);
 
-    /*
-     * Fetch the HVQM2 header
-     */
+    // Fetch the HVQM2 header
     romcpy(hvqm_header, _hvqmdataSegmentRomStart, sizeof(HVQM2Header), OS_MESG_PRI_NORMAL,
            &videoDmaMesgBlock, &videoDmaMessageQ);
-
-    /*
-     * Print the HVQM2 header information to the debugger
-     */
-    // print_hvqm_info(hvqm_header);
 
     u32 total_frames = load32(hvqm_header->total_frames);
     u32 usec_per_frame = load32(hvqm_header->usec_per_frame);
@@ -113,26 +102,17 @@ void Main(void *argument) {
     v_offset = (SCREEN_HT - hvqm_header->height) / 2;
     screen_offset = SCREEN_WD * v_offset + h_offset;
 
-    /*
-     * Setup the HVQM2 image decoder
-     */
+    // Setup the HVQM2 image decoder
     hvqm2SetupSP1(hvqm_header, SCREEN_WD);
 
-    /*
-     * Repetitive playback loop
-     */
+    // Repetitive playback loop
     int prev_bufno = -1;
     u32 video_remain = total_frames;
     u64 disptime = 0;
     void *video_streamP = _hvqmdataSegmentRomStart + sizeof(HVQM2Header);
+
     while (video_remain > 0) {
         osSyncPrintf("vremain %d\n", video_remain);
-
-        /*
-         * Release all frame buffers
-         */
-        release_all_cfb();
-
 
         u8 header_buffer[sizeof(HVQM2Record) + 16];
         HVQM2Record *record_header;
@@ -146,99 +126,75 @@ void Main(void *argument) {
         video_streamP = get_record(record_header, hvqbuf, HVQM2_VIDEO, video_streamP,
                                    &videoDmaMesgBlock, &videoDmaMessageQ);
 
-        {
-            /*
-             *   This block is an example of how to force the video to play
-             * in sync with the audio.
-             *
-             *   The video and audio is synchronized and played back by the
-             * timekeeper thread, but this assumes the video (frame buffer)
-             * is always completed and sent to the timekeeper thread before
-             * its scheduled display time.
-             *
-             *   But with mixed I/O the compressed data can be read late and
-             * decoding can take more time due to the increased burden on the
-             * CPU, leading to a situation where the video can become delayed
-             * relative to the audio.
-             *
-             *   One way to counter this is to skip to the next keyframe
-             * whenever the frame to be decoded is late by an amount of
-             * time equal to 2 or more frames.
-             *
-             */
-            if (disptime > 0) { /* Excluding the first frame */
-                if (tkGetTime() > (disptime + (usec_per_frame * 2))) {
-                    release_all_cfb();
-                    do {
-                        disptime += usec_per_frame;
-                        if (--video_remain == 0)
-                            break;
-                        video_streamP =
-                            get_record(record_header, hvqbuf, HVQM2_VIDEO, video_streamP,
-                                       &videoDmaMesgBlock, &videoDmaMessageQ);
-                    } while (load16(record_header->format) != HVQM2_VIDEO_KEYFRAME
-                             || tkGetTime() > disptime);
-                    if (video_remain == 0)
+        /*
+         *   This block is an example of how to force the video to play
+         * in sync with the audio.
+         *
+         *   The video and audio is synchronized and played back by the
+         * timekeeper thread, but this assumes the video (frame buffer)
+         * is always completed and sent to the timekeeper thread before
+         * its scheduled display time.
+         *
+         *   But with mixed I/O the compressed data can be read late and
+         * decoding can take more time due to the increased burden on the
+         * CPU, leading to a situation where the video can become delayed
+         * relative to the audio.
+         *
+         *   One way to counter this is to skip to the next keyframe
+         * whenever the frame to be decoded is late by an amount of
+         * time equal to 2 or more frames.
+         *
+         */
+        if (disptime > 0) { // Excluding the first frame
+            if (getTime() > (disptime + (usec_per_frame * 2))) {
+                do {
+                    disptime += usec_per_frame;
+                    if (--video_remain == 0)
                         break;
-                }
+                    video_streamP =
+                        get_record(record_header, hvqbuf, HVQM2_VIDEO, video_streamP,
+                                   &videoDmaMesgBlock, &videoDmaMessageQ);
+                } while (load16(record_header->format) != HVQM2_VIDEO_KEYFRAME
+                         || getTime() > disptime);
+                if (video_remain == 0)
+                    break;
             }
         }
 
-        /*
-         * Decode the compressed image data and expand it in the frame buffer
-         */
+        // Decode the compressed image data and expand it in the frame buffer
         frame_format = load16(record_header->format);
 
         if (frame_format == HVQM2_VIDEO_HOLD) {
-            /*
-             *   Just like when frame_format != HVQM2_VIDEO_HOLD you
-             * could call hvqm2Decode*() and decode in a new frame
-             * buffer (in this case, just copying from the buffer of
-             * the preceding frame).  But here we make use of the
-             * preceding frame's buffer for the next frame in order
-             * to speed up the process.
-             */
+           // do nothing
         } else {
-            // bufno = get_cfb(); /* Get the frame buffer */
+            int status;
+            // Process first half in the CPU
+            hvqtask.t.flags = 0;
+            status = hvqm2DecodeSP1(hvqbuf, frame_format, &cfb[bufno][screen_offset],
+                                    &cfb[prev_bufno][screen_offset], hvqwork, &hvq_sparg,
+                                    hvq_spfifo);
+            osWritebackDCacheAll();
 
-            /* If using the RSP version of the decoder */
-            {
-                int status;
-
-                /*
-                 * Process first half in the CPU
-                 */
-                hvqtask.t.flags = 0;
-                status = hvqm2DecodeSP1(hvqbuf, frame_format, &cfb[bufno][screen_offset],
-                                        &cfb[prev_bufno][screen_offset], hvqwork, &hvq_sparg,
-                                        hvq_spfifo);
-                osWritebackDCacheAll();
-
-                /*
-                 * Process last half in the RSP
-                 */
-                if (status > 0) {
-                    osInvalDCache((void *) cfb[bufno], sizeof cfb[bufno]);
-                    osSpTaskStart(&hvqtask);
-                    osRecvMesg(&spMesgQ, NULL, OS_MESG_BLOCK);
-                }
+            // Process last half in the RSP
+            if (status > 0) {
+                osInvalDCache((void *) cfb[bufno], sizeof cfb[bufno]);
+                osSpTaskStart(&hvqtask);
+                osRecvMesg(&spMesgQ, NULL, OS_MESG_BLOCK);
             }
         }
-        /*
-         * Don't need the previous frame anymore, so release it
-         */
-        osViSwapBuffer(cfb[bufno]);
-        
+
         if (frame_format != HVQM2_VIDEO_HOLD) {
+            osViSwapBuffer(cfb[bufno]);
+        
             prev_bufno = bufno++;
             if (bufno >= NUM_CFBs) {
                 bufno = 0;
             }
+        } else {
+            // TODO: wait for next frame
         }
 
-        /*
-         * Go to the process for next frame
-         */
+        // Go to the process for next frame
         disptime += usec_per_frame;
         --video_remain;
     }
